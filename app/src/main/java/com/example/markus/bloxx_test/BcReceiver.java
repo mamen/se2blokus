65a1ba08330444ccfe4c7041d59dbdfd07ce3926
Markus Mennel <markusme@edu.aau.at>
2016-04-19 22:49:07 +0200

    fragments_test

---
+++ app/src/main/java/com/example/markus/bloxx_test/BcReceiver.java
@@ -0,0 +1,32 @@
+package com.example.markus.bloxx_test;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+
+/**
+ * Created by Markus on 19.04.2016.
+ */
+public class BcReceiver extends android.content.BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Determine if Wifi P2P mode is enabled or not, alert
+            // the Activity.
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+            // The peer list has changed!  We should probably do something about
+            // that.
+
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+
+            // Connection state changed!  We should probably do something about
+            // that.
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/markus/bloxx_test/DeviceListFragment.java
@@ -0,0 +1,10 @@
+package com.example.markus.bloxx_test;
+
+import android.app.Fragment;
+
+/**
+ * Created by Markus on 19.04.2016.
+ */
+public class DeviceListFragment extends Fragment{
+
+}
