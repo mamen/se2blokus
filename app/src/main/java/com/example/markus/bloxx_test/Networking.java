3d7267a98bed04cdb1839967cdf3157cf42407a8
Markus Mennel <markusme@edu.aau.at>
2016-04-19 17:25:29 +0200

    initial commit of project

---
+++ app/src/main/java/com/example/markus/bloxx_test/Networking.java
@@ -0,0 +1,15 @@
+package com.example.markus.bloxx_test;
+
+/**
+ * Created by Markus on 18.04.2016.
+ */
+public class Networking {
+
+    public int host(){
+        return 0;
+    }
+
+    public int connect(){
+        return 0;
+    }
+}
