f8e19332be520fa74f4ea7804c0e1c4faa40102b
Markus Mennel <markusme@edu.aau.at>
2016-04-20 23:51:43 +0200

    Revert "fragments_test"
    This reverts commit 65a1ba08330444ccfe4c7041d59dbdfd07ce3926.

--- app/src/main/java/com/example/markus/bloxx_test/Startscreen.java
+++ app/src/main/java/com/example/markus/bloxx_test/Startscreen.java
@@ -1,23 +1,19 @@
 package com.example.markus.bloxx_test;
 
 import android.annotation.SuppressLint;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Context;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class Startscreen extends AppCompatActivity implements View.OnClickListener {
+public class Startscreen extends AppCompatActivity {
 
     /**
      * Some older devices needs a small delay between UI widget updates
@@ -28,64 +24,14 @@ public class Startscreen extends AppCompatActivity implements View.OnClickListen
     private View mContentView;
     private View mControlsView;
 
-    Button btn_joinGame;
-    Button btn_hostGame;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_startscreen);
 
-        btn_joinGame = (Button) findViewById(R.id.joinGame);
-        btn_joinGame.setOnClickListener(this);
-        btn_hostGame = (Button) findViewById(R.id.hostGame);
-        btn_hostGame.setOnClickListener(this);
-
+        mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
-    }
-
-    public void onClick(View v) {
-        Context context = getApplicationContext();
-        int duration = Toast.LENGTH_SHORT;
-        CharSequence text;
-        Toast toast;
-        Fragment slFrag;
-        FragmentTransaction ft;
-        FragmentManager fm;
-        Fragment fragOne = new ServerlistFragment();
-
-
-        switch (v.getId()) {
-            case R.id.joinGame:
-                text = "joinGame";
-                toast = Toast.makeText(context, text, duration);
-                toast.show();
-
-                fm = getFragmentManager();
-                ft = fm.beginTransaction();
-                fm.beginTransaction();
-                ft.show(fragOne);
-
-                break;
-            case R.id.hostGame:
-                text = "hostGame";
-                toast = Toast.makeText(context, text, duration);
-                toast.show();
-
-                fm = getFragmentManager();
-                ft = fm.beginTransaction();
-
-                fm.beginTransaction();
-                // it's wrong , you just hide the fragment that not added to  FragmentTransactio
-                ft.hide(fragOne);
-
-                break;
-        }
-                /*fm.beginTransaction()
-                        .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
-                        .show(fm.findFragmentById(R.layout.fragment_serverlist))
-                        .commit();*/
     }
 
     @Override
