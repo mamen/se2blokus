5acd8944dacc84309f5e541606ee1eb5d01fdd79
ManuelSeywald <manuel.seywald@aau.at>
2016-05-09 19:04:15 +0200

    Game Rules
    Game Rules should work now. First Stone placement should work for every
    stone. Will be tested tho

--- app/src/main/java/at/aau/se2/test/FullscreenActivity.java
+++ app/src/main/java/at/aau/se2/test/FullscreenActivity.java
@@ -34,7 +34,6 @@ public class FullscreenActivity extends Activity {
     private List<ImageView> blockDrawer_children;
     private List<ImageView> removed_blockDrawer_children;
     private byte playerID;
-    private static boolean firstrm = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -198,7 +197,7 @@ public class FullscreenActivity extends Activity {
                         oImageView.setImageResource(R.drawable.gameboard_empty);
                         oImageView.setOnClickListener(new View.OnClickListener() {
                             public void onClick(View v) {
-                                checkGameRules(v);
+                                isYourPlacementValid(v);
                             }
                         });
                         break;
@@ -223,67 +222,33 @@ public class FullscreenActivity extends Activity {
             Toast.makeText(getApplicationContext(), ("First goes in the corner"), Toast.LENGTH_SHORT).show();
     }
 
-    /*
-    * Actual New Stone Placement:
-    **  Check Stone Placement:
-    ** - if(placerOverEdge) stone reaches over field
-    ** - if(hitSomeStones) stone hits another block
-    ** - check for gamerules here!? if(removed_blockDrawer_children.isEmpty())
-    **   -> firstStone -> corner, etc.
-    * */
-    private void checkGameRules(View v) {
+
+    private void isYourPlacementValid(View v) {
         if (selectedBlockID >= 0) {
             for (int i = 0; i < gameBoardLayout.getColumnCount(); i++) {
                 for (int j = 0; j < gameBoardLayout.getRowCount(); j++) {
                     if (gameBoardLayout.getChildAt((i * SIZE) + j).equals(v)) {
                         byte[][] b;
                         b = blocks.getStone(selectedBlockID - 1);
-                        //If single Stone, just place it
                         if (!removed_blockDrawer_children.isEmpty()) {
-                            if (selectedBlockID != 0) {
-                                if (placeOverEdge(b, i, j)) {
-                                    Toast.makeText(getApplicationContext(), ("You can't place that here"), Toast.LENGTH_SHORT).show();
-                                    break;
-                                } else if (hitSomeStones(b, i, j)) {
-                                    Toast.makeText(getApplicationContext(), ("You can't place that here"), Toast.LENGTH_SHORT).show();
-                                    break;
-                                } else {
-                                    placeStone(b, i, j);
-                                }
+                            if (checkTheRules(b, i, j)) {
+                                placeStone(b, i, j);
+                                removeFromBlockDrawer();
                             } else {
-                                gl.setSingleStone(b[0][0], i, j);
+                                break;
                             }
                         } else {
                             if (hitTheCorner(b, i, j)) {
                                 placeStone(b, i, j);
-                                firstrm = true;
+                                removeFromBlockDrawer();
                             }
-                        }
-
-                        // Counting goes right, selectedBlockID should be same than Tag.
-                        int count = 0;
-                        if (!removed_blockDrawer_children.isEmpty()) {
-                            for (ImageView t : removed_blockDrawer_children) {
-                                if ((int) t.getTag() < selectedBlockID) {
-                                    count++;
-                                }
-                            }
-                        }
-
-                        if (firstrm) {
-                            int rm_index = Math.max(0, (selectedBlockID) - count);
-                            ImageView rm = (ImageView) blockDrawer.getChildAt(rm_index);
-                            //Toast.makeText(getApplicationContext(), (selectedBlockID) + " / " + rm_index + " / " + (22 - blockDrawer.getChildCount()), Toast.LENGTH_SHORT).show();
-                            blockDrawer.removeView(rm);
-                            blockDrawer_children.remove(rm);
-                            removed_blockDrawer_children.add(rm);
-                            selectedBlockID = -1;
                         }
                     }
                 }
             }
             updateGameBoard();
         }
+
     }
 
     private void vibrate() {
@@ -303,15 +268,15 @@ public class FullscreenActivity extends Activity {
      * Checks if you place over the edges
      *
      * @param b - the byte array of your stone
-     * @param i - the row where you want to place it
-     * @param j - the col where you want to place it
+     * @param i - the col where you want to place it
+     * @param j - the row where you want to place it
      * @return true, if your stone reaches over the board
      * false, else
      */
     private boolean placeOverEdge(byte[][] b, int i, int j) {
-        for (int row = i; row < i + b.length; row++) {
-            for (int col = j; col < j + b.length; col++) {
-                if (b[col - j][row - i] != 0 && (row >= SIZE || col >= SIZE)) {
+        for (int col = i; col < i + b.length; col++) {
+            for (int row = j; row < j + b.length; row++) {
+                if (b[row - j][col - i] != 0 && (col >= SIZE || row >= SIZE)) {
                     return true;
                 }
             }
@@ -324,16 +289,16 @@ public class FullscreenActivity extends Activity {
      * Checks if you hit other stones with your placement
      *
      * @param b - the byte array of your stone
-     * @param i - the row where you want to place it
-     * @param j - the col where you want to place it
+     * @param i - the col where you want to place it
+     * @param j - the row where you want to place it
      * @return true, if you would hit other stones with this move
      * false, else
      */
     private boolean hitSomeStones(byte[][] b, int i, int j) {
         byte[][] board = gl.getGameBoard();
-        for (int row = i; row < i + b.length; row++) {
-            for (int col = j; col < j + b.length; col++) {
-                if (b[col - j][row - i] != 0 && board[row][col] != 0) {
+        for (int col = i; col < i + b.length; col++) {
+            for (int row = j; row < j + b.length; row++) {
+                if (b[row - j][col - i] != 0 && board[col][row] != 0) {
                     return true;
                 }
             }
@@ -344,26 +309,142 @@ public class FullscreenActivity extends Activity {
 
     /**
      * Method for "First Stone to Corner"-Rule -- needs to be adapted to 1 vs. 1 because of two colors
+     * Should be working for all stones now
      *
      * @param b - the byte array of your stone
-     * @param i - the row where you want to place it
-     * @param j - the col where you want to place it
+     * @param i - the col where you want to place it
+     * @param j - the row where you want to place it
      * @return true, if stone fits in the corner
      * false, if stone is not in the corner
      */
     private boolean hitTheCorner(byte[][] b, int i, int j) {
         if (!placeOverEdge(b, i, j) && !hitSomeStones(b, i, j)) {
-            for (int bRow = 0; bRow < b.length; bRow++) {
-                for (int bCol = 0; bCol < b.length; bCol++) {
-                    if (b[bRow][bCol] != 0 &&
-                            ((i + bRow) == 0 && (j + bCol) == 0) ||
-                            ((i + bRow) == SIZE - 1 && (j + bCol) == 0) ||
-                            ((i + bRow) == 0 && (j + bCol) == SIZE - 1) ||
-                            ((i + bRow) == SIZE - 1 && (j + bCol) == SIZE - 1)) {
-                        return true;
+            byte[][] board = gl.getGameBoard();
+            for (int col = i; col < i + b.length; col++) {
+                for (int row = j; row < j + b.length; row++) {
+                    if (b[row - j][col - i] != 0) {
+                        if ((col == 0 && row == 0) ||
+                                (col == 0 && row == SIZE - 1) ||
+                                (col == SIZE - 1 && row == 0) ||
+                                (col == SIZE - 1 && row == SIZE - 1)) {
+                            return true;
+                        }
                     }
                 }
             }
+            return false;
+        } else {
+            Toast.makeText(getApplicationContext(), ("Why does this fail?"), Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    /**
+     * Checks the game rules, if your stone touches your color only by corner.
+     * Also works at the edges, without Exceptions (as far as tested)
+     * Different behaviour for the walls and the Corners, maybe something is extractable.
+     * If-loops checks, if we touch one stone
+     * Else-If checks then, if we touch at least one corner
+     * Color sensitive
+     *
+     * @param b - the byte array of your stone
+     * @param i - the col where you want to place it
+     * @param j - the row where you want to place it
+     * @return false, if stone Placement would be invalid
+     * true, else
+     */
+
+    private boolean checkTheRules(byte[][] b, int i, int j) {
+        byte[][] board = gl.getGameBoard();
+        if (placeOverEdge(b, i, j)) {
+            return false;
+        } else if (hitSomeStones(b, i, j)) {
+            return false;
+        } else {
+            int corner = 0; //We need at least one cornerContact to make it valid
+            for (int col = i; col < i + b.length; col++) {
+                for (int row = j; row < j + b.length; row++) {
+                    if (b[row - j][col - i] != 0) {
+                        //Let the cases begin
+                            //Bottom seems fine
+                        if ((row + 1) >= SIZE && (((col + 1) < SIZE) && (col - 1) >= 0)) {
+                            if (checkSurroundings(board[col + 1][row], board[col - 1][row], board[col][row - 1])) {
+                                Toast.makeText(getApplicationContext(), ("Bottom Wall test"), Toast.LENGTH_SHORT).show();
+                                return false;
+                            } else if (checkSurroundings(board[col - 1][row - 1], board[col + 1][row - 1])) {
+                                corner++;
+                            }
+                            //Right seems fine
+                        } else if ((col + 1) >= SIZE && (((row + 1) < SIZE) && (row - 1) >= 0)) {
+                            if (checkSurroundings(board[col][row - 1], board[col - 1][row], board[col][row + 1])) {
+                                Toast.makeText(getApplicationContext(), ("Right Wall test"), Toast.LENGTH_SHORT).show();
+                                return false;
+                            } else if (checkSurroundings(board[col - 1][row - 1], board[col - 1][row + 1])) {
+                                corner++;
+                            }
+                            //Top seems fine
+                        } else if ((row - 1) < 0 && ((col - 1) >= 0 && (col + 1) < SIZE)) {
+                            if (checkSurroundings(board[col - 1][row], board[col][row + 1], board[col + 1][row])) {
+                                Toast.makeText(getApplicationContext(), ("Top Wall test"), Toast.LENGTH_SHORT).show();
+                                return false;
+                            } else if (checkSurroundings(board[col - 1][row + 1], board[col + 1][row + 1])) {
+                                corner++;
+                            }
+                            //Left seems fine
+                        } else if ((col - 1) < 0 && ((row - 1) >= 0 && (row + 1) < SIZE)) {
+                            if (checkSurroundings(board[col][row - 1], board[col + 1][row], board[col][row + 1])) {
+                                Toast.makeText(getApplicationContext(), ("Left Wall test"), Toast.LENGTH_SHORT).show();
+                                return false;
+                            } else if (checkSurroundings(board[col + 1][row - 1], board[col + 1][row + 1])) {
+                                corner++;
+                            }
+                            //Right down seems fine
+                        } else if ((row + 1) >= SIZE && (col + 1) >= SIZE) {
+                            if (checkSurroundings(board[col - 1][row], board[col][row - 1])) {
+                                Toast.makeText(getApplicationContext(), ("Right down corner test"), Toast.LENGTH_SHORT).show();
+                                return false;
+                            } else if (checkSurroundings(board[col - 1][row - 1])) {
+                                corner++;
+                            }
+                            //Right upper seems fine
+                        } else if ((row - 1) < 0 && (col + 1) >= SIZE) {
+                            if (checkSurroundings(board[col - 1][row], board[col][row + 1])) {
+                                Toast.makeText(getApplicationContext(), ("Right upper corner test"), Toast.LENGTH_SHORT).show();
+                                return false;
+                            } else if (checkSurroundings(board[col - 1][row + 1])) {
+                                corner++;
+                            }
+                            //Left down seems fine
+                        } else if ((row + 1) >= SIZE && (col - 1) < 0) {
+                            if (checkSurroundings(board[col + 1][row], board[col][row - 1])) {
+                                Toast.makeText(getApplicationContext(), ("Left down corner test"), Toast.LENGTH_SHORT).show();
+                                return false;
+                            } else if (checkSurroundings(board[col + 1][row - 1])) {
+                                corner++;
+                            }
+                            //Left upper seems fine
+                        } else if ((row - 1) < 0 && (col - 1) < 0) {
+                            if (checkSurroundings(board[col + 1][row], board[col][row + 1])) {
+                                Toast.makeText(getApplicationContext(), ("Left upper corner test"), Toast.LENGTH_SHORT).show();
+                                return false;
+                            } else if (checkSurroundings(board[col + 1][row + 1])) {
+                                corner++;
+                            }
+                        } else {
+                            //Inside field seems fine
+                            if (checkSurroundings(board[col + 1][row], board[col][row + 1], board[col - 1][row], board[col][row - 1])) {
+                                Toast.makeText(getApplicationContext(), ("That's invalid, dude!"), Toast.LENGTH_SHORT).show();
+                                return false;
+                            } else if (checkSurroundings(board[col - 1][row - 1], board[col + 1][row - 1], board[col - 1][row + 1], board[col + 1][row + 1])) {
+                                corner++;
+                            }
+                        }
+
+                    }
+                }
+            }
+            if (corner != 0)
+                return true;
         }
         return false;
     }
@@ -373,8 +454,8 @@ public class FullscreenActivity extends Activity {
      * places the stone, byte by byte
      *
      * @param b - the byte array of your stone
-     * @param i - the row where you want to place it
-     * @param j - the col where you want to place it
+     * @param i - the col where you want to place it
+     * @param j - the row where you want to place it
      */
     private void placeStone(byte[][] b, int i, int j) {
         for (int x = 0; x < b.length; x++) {
@@ -384,5 +465,43 @@ public class FullscreenActivity extends Activity {
                 }
             }
         }
+    }
+
+
+    /**
+     * Removes the used stone from the View
+     */
+    private void removeFromBlockDrawer() {
+        int count = 0;
+        if (!removed_blockDrawer_children.isEmpty()) {
+            for (ImageView t : removed_blockDrawer_children) {
+                if ((int) t.getTag() < selectedBlockID) {
+                    count++;
+                }
+            }
+        }
+
+        int rm_index = Math.max(0, (selectedBlockID) - count);
+        ImageView rm = (ImageView) blockDrawer.getChildAt(rm_index);
+        //Toast.makeText(getApplicationContext(), (selectedBlockID) + " / " + rm_index + " / " + (22 - blockDrawer.getChildCount()), Toast.LENGTH_SHORT).show();
+        blockDrawer.removeView(rm);
+        blockDrawer_children.remove(rm);
+        removed_blockDrawer_children.add(rm);
+        selectedBlockID = -1;
+    }
+
+
+    /**
+     * Checks, if the surroundings are colored or not
+     *
+     * @param surroundings - bytes representing the color of the player
+     * @return true, if one of the surroundings matches the current playerColor
+     * false, else
+     */
+    public boolean checkSurroundings(byte... surroundings) {
+        for (byte check : surroundings) {
+            if (check == playerID) return true;
+        }
+        return false;
     }
 }
