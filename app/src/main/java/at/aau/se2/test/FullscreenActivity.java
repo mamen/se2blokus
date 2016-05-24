c19108d96ecc5fabab56d41b7a283926acd33d4c
Sh4wNy <markusme@edu.aau.at>
2016-05-24 14:55:41 +0200

    testcommit

--- app/src/main/java/at/aau/se2/test/FullscreenActivity.java
+++ app/src/main/java/at/aau/se2/test/FullscreenActivity.java
@@ -17,7 +17,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.List;
 
