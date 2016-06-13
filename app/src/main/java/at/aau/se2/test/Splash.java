718ef99cdee78c9bbae70a17d6febca73a998023
Sh4wNy <markusme@edu.aau.at>
2016-06-13 19:13:01 +0200

    added:  can only start app, if connected to WiFi (include start WiFi dialog)

--- app/src/main/java/at/aau/se2/test/Splash.java
+++ app/src/main/java/at/aau/se2/test/Splash.java
@@ -1,8 +1,14 @@
 package at.aau.se2.test;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -25,11 +31,16 @@ public class Splash extends Activity {
         txt_logo.setTypeface(font);
         txt_logo.setTextSize(90);
 
-        Thread timer = new Thread() {
+        Thread checkWiFiThread = new Thread() {
+            @Override
             public void run() {
                 try {
+                    //wait for connection
+                    while (!isConnectedToNetwork()) {
+                        sleep(1);
+                    }
                     sleep(1000);
-                } catch (InterruptedException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
                     Intent openStart = new Intent("at.aau.se2.test.STARTSCREEN");
@@ -37,6 +48,50 @@ public class Splash extends Activity {
                 }
             }
         };
-        timer.start();
+
+        checkWiFiThread.start();
+        if(!isConnectedToNetwork()){
+            showWiFiDialog();
+        }
+
     }
+
+    private void showWiFiDialog(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Splash.this);
+
+        // set title
+        alertDialogBuilder.setTitle("No WiFi-connection");
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage(R.string.splash_nowificonnection)
+                .setCancelable(false)
+                .setPositiveButton(R.string.splash_yes,new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        //enable wifi
+                        WifiManager wifiManager = (WifiManager)getApplication().getSystemService(Context.WIFI_SERVICE);
+                        wifiManager.setWifiEnabled(true);
+                    }
+                })
+                .setNegativeButton(R.string.splash_no,new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        //disable wifi
+                        System.exit(0);
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+
+    private boolean isConnectedToNetwork() {
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        return mWifi.isConnected();
+    }
+
 }
